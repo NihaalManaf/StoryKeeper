fd6336c1f0168d0a6f20403b6c9b4e2a3758573d
nihaalmanaf <nihaalmanaf@gmail.com>
2025-03-23 20:10:56 +0800

    included analytics

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,10 +6,12 @@ import PricingSection from "@/components/PricingSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import FaqSection from "@/components/FaqSection";
 import CtaSection from "@/components/CtaSection";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <div className="bg-white">
+      <Analytics />
       <HeroSection />
       <HowItWorks />
       <SampleBooks />
